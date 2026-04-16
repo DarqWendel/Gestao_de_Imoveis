ca7adb5852cb052f18656fef4aacdd0b588b6b9c
DarqWendel <darquiowendel2021@gmail.com>
2026-04-16 03:55:04 -0300

    arrumando

--- api/index.js
+++ api/index.js
@@ -8,8 +8,15 @@ app.use(express.json());
 app.use(cors());
 
 const db = mysql.createConnection({
-    uri: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    ssl: {
+        ca: process.env.DB_SSL_CERT,
+        rejectUnauthorized: true
+    }
 });
 
 app.post('/api/registrar', (req, res) => {
